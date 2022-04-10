38b365fba146451c1a70082c474c62b364cc4071
seanrutto <travisean.ts@gmail.com>
2022-04-10 15:09:28 +0300

    New change

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,22 +4,22 @@
 /**
  * main - Entry point of program
  *
- * function: assign randomm number to  n each time it is executed
+ * Function: assign randomm number to  n each time it is executed
  *
- * return: always Zero
+ * Return: always Zero (0)
  */
 int main (void)
 {
 	int n;
 
 	srand(time(0));
-	n=rand() - RAND_MAX / 2;
-	if(n < 0){
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
 		printf("%d is negative\n", n);
-	}else if(n > 0){
+	else if (n > 0)
 		printf("%d is positive\n", n);
-	}else{
+        else
 		printf("%d is zero\n", n);
-	}
+	
 	return (0);
 }
