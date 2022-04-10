396d6eff8f45a362ccaaa4c0b96ad1d4a4d72d04
seanrutto <travisean.ts@gmail.com>
2022-04-10 21:23:30 +0300

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * Main - is beginnig of function
+ *
+ * Function: Prints numbers to nine
+ *
+ * Return: is always 0
+ */
+int main(void)
+{
+	int x;
+	for (x=0; x<10; x++)
+		printf("%d",x);
+	return (0);
+}
