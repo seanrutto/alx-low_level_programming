465547037b699a626875894e66ac88c287c4916e
seanrutto <travisean.ts@gmail.com>
2022-04-10 21:46:51 +0300

    SAwd

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * Main - starts the prog
+ *
+ * Function: prints numbers
+ *
+ * Return: is 0 as always
+ */
+int main(void)
+{
+	int x;
+	for (x=0; x<0; x++)
+		putchar("%d",x);
+	putchar("\n");
+	return (0);
+}
