eb0c76de1fbe3ac19c431f0f5a7b5efbeb6ecadc
seanrutto <travisean.ts@gmail.com>
2022-04-11 16:37:15 +0300

    Yuyrso

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * Main - function begins
+ *
+ * Function: prints some numbers
+ *
+ * Return: is always 0
+ */
+int main(void)
+{
+	int x;
+	for (x='0'; x<='9'; x++)
+	{
+		putchar(x);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
