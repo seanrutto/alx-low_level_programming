9300b5e5b10384e09500d0e96dc8b2ff7ec379d5
seanrutto <travisean.ts@gmail.com>
2022-04-11 15:32:58 +0300

    Awder

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * Main - starts function
+ *
+ * Function: Prints z to a
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char letter;
+	for (letter='z'; letter<='a'; letter--)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
