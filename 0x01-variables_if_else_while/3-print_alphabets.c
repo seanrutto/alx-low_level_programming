08b10924b0c68214b4b1869313adb4bb34665815
seanrutto <travisean.ts@gmail.com>
2022-04-11 19:40:41 +0300

    Ontyy

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,9 +12,9 @@ int main(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar (letter);
-        for (letter = 'A'; letter <= 'Z'; letter++)
-                putchar (letter);
-        putchar ('\n');
-        return (0);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar (letter);
+	putchar ('\n');
+	return (0);
 }
 
