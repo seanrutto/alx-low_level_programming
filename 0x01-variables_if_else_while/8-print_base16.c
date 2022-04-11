b4699fda7abd7e9acacbf697a5019f90cf208fb9
seanrutto <travisean.ts@gmail.com>
2022-04-11 16:08:52 +0300

    Asedw

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * main - prog begins
+ *
+ * Function: prints a few things
+ *
+ * Return: alwways 0
+ */
+int main(void)
+{
+	int x;
+	char letters;
+	for (x='0'; x<='9'; x++)
+		putchar(x);
+	for (letters='a';letters<='f'; letters++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
