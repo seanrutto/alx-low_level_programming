054eea025e57eae33e1b0736f9b607456b5d3e55
seanrutto <travisean.ts@gmail.com>
2022-04-12 15:25:26 +0300

    First try

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+int main(void)
+{
+	char c;
+	int i;
+	c[7] = {"-","p","u","t","c","h","a","r"};
+
+	for (i=0; i<=7; i++)
+		_putchar(c[i]);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+In this directory we have questions on functions 
+Eleven mandatory and five optional
