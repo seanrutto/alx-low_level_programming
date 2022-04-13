35af9c744bac4a66142ed3c8097762529d230a26
seanrutto <travisean.ts@gmail.com>
2022-04-13 15:33:37 +0300

    Asedfse

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int print_sign(int c)
+{
+	if (c > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (c == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else (c < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
