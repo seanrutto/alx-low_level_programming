7b19d84f666b88bb8c286e2911fba04ae72fe48a
seanrutto <travisean.ts@gmail.com>
2022-04-13 20:55:16 +0300

    Fourthree

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * _islower - check the code for lowercase letter
+ *@c is an ineger by user
  *
  * Return: Always 0.
  */
@@ -9,7 +10,7 @@ int _islower(int c)
 {
 
 
-	if(c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 		return (1);
 
 	return (0);
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * _isalpha - checks the code for an alphabet
+ *@c is an integer by user
  *
  * Return: Always 0.
  */
 int _isalpha(int c)
 {
 
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
 
-        if((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-                return (1);
-
-        return (0);
+	return (0);
 
 
 }
