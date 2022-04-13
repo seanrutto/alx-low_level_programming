5dd58381dc59b39d7fd213854a35b0025209e81c
seanrutto <travisean.ts@gmail.com>
2022-04-13 18:00:57 +0300

    Asefwfv

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,18 +7,18 @@
  */
 void jack_bauer(void)
 {
-	int a = '0'; 
-	int b = '0';
-	int c = '0';
-	int d = '0';
+	int a; 
+	int b;
+	int c;
+	int d;
 
-	for (a = 0; a <= 2; a++)
+	for (a = '0'; a <= '2'; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = '0'; b <= '9'; b++)
 		{
-			for (c = 0; c <= 5; c++)
+			for (c = '0'; c <= '5'; c++)
 			{
-				for (d = 0; d <= 9; d++)
+				for (d = '0'; d <= '9'; d++)
 				{
 					_putchar (a);
 					_putchar (b);
