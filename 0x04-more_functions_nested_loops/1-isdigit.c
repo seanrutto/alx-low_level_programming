42e6537b44d9cd88446aad2b2e090acd0c40d4a3
seanrutto <travisean.ts@gmail.com>
2022-04-19 19:35:17 +0300

    One to three

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /*
- *
- *
- *
- * 
+ * _isdigit - if c is a digit
+ * @c: the integer being checked
+ * Return: Always 0.
  */
+
 int _isdigit(int c)
 {
 	if (c > 47 && c < 58)
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/*
+ * mul - multiply integers
+ * @a: print int
+ * @b: print int
+ *  Return: is always 0.
+ */
+
+int mul(int a, int b)
+{
+	return (a*b);
+}
