5ebc356b6fec9ac360fb67f5cd3bbb0ca75a7d48
seanrutto <travisean.ts@gmail.com>
2022-04-19 17:55:48 +0300

    Adrfw

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
-/*
+/* _isupper - checks if letter is upppercase
+ * @c: the integer to check
  *
- *
+ * 
+ * Return: is always 0
  */
 
 int _isupper(int c)
 {
-	if (c > 65 && c < 91)
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#include 
+#include <unistd.h> 
 
 int _isupper(int c);
 int _isdigit(int c);
