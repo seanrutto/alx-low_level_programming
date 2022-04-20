31f50ae5085134f2233b2a8b6b4706027f359d7c
seanrutto <travisean.ts@gmail.com>
2022-04-20 22:12:13 +0300

    Nine fixed

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,10 +13,10 @@ char *_strcpy(char *dest, char *src)
 	i = 0;
 	while (src[i] != '\0')
 	{
-		dest[i] = src [i];
+		dest[i] = src[i];
 		i++;
 	}
-	dest [i] = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
