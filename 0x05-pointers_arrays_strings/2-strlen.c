6f7a73a20015c308af426b89f9fc9605e46e152e
seanrutto <travisean.ts@gmail.com>
2022-04-20 22:04:10 +0300

    Number two fix

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strlen - returns the length of a string
+ * _strlen - returns the length of a string
  * @s: pointer to our string that will changed
  *
  * Return: void means our answer is correct.
