3df6b6187262f106dcd4cd3326ed098dcddf877a
seanrutto <travisean.ts@gmail.com>
2022-04-20 22:23:07 +0300

    Number six fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,11 +11,11 @@ void puts2(char *str)
 {
 	int a;
 
-	for (a = 0; str[a] != '\o'; a++)
+	for (a = 0; str[a] != '\0'; a++)
 	{
 		if (a % 2 == 0)
 			_putchar (str[a]);
 	}
 
-	_putchar ('\n')
+	_putchar ('\n');
 }
