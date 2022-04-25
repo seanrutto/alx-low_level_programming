3db09fc9f46a2375d6150b3533b1d7023316782c
seanrutto <travisean.ts@gmail.com>
2022-04-25 17:42:57 +0300

    Question 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -20,9 +20,9 @@ char *_strncat(char *dest, char *src, int n)
 		c++;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[c + 1] = src[i];
+		dest[c + i] = src[i];
 
-	dest[c + 1] = '\0';
+	dest[c + i] = '\0';
 
 	return (dest);
 }
