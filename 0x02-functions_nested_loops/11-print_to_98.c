d299de71693e920fddb137ff1a9927c1fc924ffb
seanrutto <travisean.ts@gmail.com>
2022-06-22 18:33:31 +0300

    Loopy

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,19 +11,19 @@
 void print_to_98(int n)
 {
 	char b= n + '0';
-	if (n <= 98 && n > 0)
+	while (n <= 98 && n > 0)
 	{
 		_putchar (b);
 		n++;
 	}
 
-	else if (n <= 0)
+	while (n <= 0)
 	{
 		_putchar (b*-1);
 		n++;
 	}
 
-	else if (n > 98)
+	while (n > 98)
 	{
 		_putchar (b);
 		n--;
