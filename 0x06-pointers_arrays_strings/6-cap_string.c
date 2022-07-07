30566eca0f14cd0ce4f3775a83873dac6f7ff99a
seanrutto <travisean.ts@gmail.com>
2022-07-07 22:27:19 +0300

    Bonus

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,3 +3,21 @@
 /**
  * cap_string - capitalizes all words of string
  * @str: string being capitalized.
+ *
+ * return: return a string with capitalized letters
+ */
+
+char *cap_string(char *str)
+{
+	int leter = 0;
+
+	while (str[letter])
+	{
+		while (!(str[letter] >= 'a' && str[letter] <= 'z'))
+			letter++;
+
+		if (str[letter - 1] == ' ' || str[letter - 1] == '\t' || str[letter - 1] == '\n' || str[letter - 1] == ',' || str[letter - 1] == ';' || str[letter - 1] == '.' || str[letter - 1] == '!' || str[letter - 1] == '?' || str[letter - 1] == '"' || str[letter - 1] == '(' || str[letter - 1] == ')' || str[letter - 1] == '{' || str[letter - 1] == '}' || letter == 0)
+			str[letter] -= 32;
+		letter ++;
+	}
+}
